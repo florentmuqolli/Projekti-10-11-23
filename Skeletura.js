be4059dfe3f15fd0da1a924a863ad9a844d7829e
florentmuqolli <149383933+florentmuqolli@users.noreply.github.com>
2023-12-07 23:51:14 +0100

    finishing touches

--- Skeletura.js
+++ Skeletura.js
@@ -24,11 +24,11 @@ HekenX.addEventListener('click', ()=>
     boxi.classList.remove('active-kycja');
 });
 
-
+let nameRegex = /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
 let emailRegex= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
-let passwordRegex= /^[a-zA-Z]\w{3,14}$/;
+let passwordRegex= /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-function validateForm() {
+function kyciratu() {
     let emailError=document.getElementById('emailgabim');
     let emailInput = document.getElementById('email');
     let passwordInput=document.getElementById('passi');
@@ -47,6 +47,36 @@ function validateForm() {
         return;
     }
 
-    alert('Succesfully');
+    alert('Logging you in :)');
+}
+
+function registeratu() {
+    let emailError=document.getElementById('emailgabimr');
+    let emailInput = document.getElementById('emailr');
+    let passwordInput=document.getElementById('passir');
+    let passwordError = document.getElementById('passigabimr');
+    let emriError = document.getElementById('emrigabim');
+    let emriInput = document.getElementById('emri');
+
+    emailError.innerText = '';
+    passwordError.innerText = '';
+    emriError.innerText = '';
+
+    if(!nameRegex.test(emriInput.value))
+    {
+        emriError.innerText='This username is not allowed';
+    }
+
+    if(!emailRegex.test(emailInput.value)) {
+        emailError.innerText='Invalid email';
+        return;
+    }
+
+    if(!passwordRegex.test(passwordInput.value)) {
+        passwordError.innerText='Invalid password';
+        return;
+    }
+
+    alert('Your data has been saved');
 }
 
