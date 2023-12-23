70bfdf726e76475f1e7a06de826d15f4fd535711
florentmuqolli <149383933+florentmuqolli@users.noreply.github.com>
2023-12-23 21:53:15 +0100

    Ndryshime

---
+++ Kontakt.js
@@ -0,0 +1,4 @@
+function hom()
+{
+    window.location = "http://127.0.0.1:5500/skeletura-main.html";
+}
--- skeletura-main.js
+++ skeletura-main.js
@@ -1,12 +1,4 @@
-const boxii = document.querySelector('.boxii');
-const kontakt = document.querySelector('.kontaktona');
-const hekenXX = document.querySelector('.hekenXX');
-
-kontakt.addEventListener('click', ()=>
+function con()
 {
-    boxii.classList.add('active-kontaktona');
-});
-hekenXX.addEventListener('click', ()=>
-{
-    boxii.classList.remove('active-kontaktona');
-});
+    window.location = "http://127.0.0.1:5500/Kontakt.html";
+}
