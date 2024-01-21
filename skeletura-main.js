4cc21b96439c5a3139b18ea7402e964706f114fa
erionrexhaj <erionrexhaj5@gmail.com>
2024-01-22 00:31:19 +0100

    Erion's commit
    Slideri

--- skeletura-main.js
+++ skeletura-main.js
@@ -2,3 +2,17 @@ function con()
 {
     window.location = "http://127.0.0.1:5500/Kontakt.html";
 }
+
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth"})
+        })
+    })
+}
+
+window.addEventListener("load", initSlider);
