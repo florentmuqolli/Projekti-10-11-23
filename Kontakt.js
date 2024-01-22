47d5fd3457eba98e8f4a7c9e165941ced32076fb
florentmuqolli <149383933+florentmuqolli@users.noreply.github.com>
2024-01-22 16:58:48 +0100

    f's

--- Kontakt.js
+++ Kontakt.js
@@ -2,3 +2,10 @@ function hom()
 {
     window.location = "http://127.0.0.1:5500/skeletura-main.html";
 }
+
+var teksti = "This is an automated mesage: Your request has been submited, a ticket id will be sent to you throw email. Thank you!";
+
+function submiti()
+{
+    alert(teksti);
+}
