b343418b8b8d215eb5dd802e9e832bb91df6c230
erionrexhaj <erionrexhaj5@gmail.com>
2024-01-26 23:22:22 +0100

    eri

--- login.php
+++ login.php
@@ -8,6 +8,28 @@
 </head>
 <body>
     <div class="container">
+        <?php
+        if (isset($_POST["login"])) {
+           $email = $_POST["email"];
+           $password = $_POST["password"];
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if ($user) {
+                if (password_verify($password, $user["password"])) {
+                    session_start();
+                    $_SESSION["user"] = "yes";
+                    header("Location: skeletura-main.php");
+                    die();
+                }else{
+                    echo "<div class='error-message'>Password is incorrect</div>";
+                }
+            }else{
+                echo "<div class='error-message'>Email is incorrect </div>";
+            }
+        }
+        ?>
         <form action="login.php" method="post">
             <div class="form-group">
                 <input type="email" name="email" class="form-ctrl" placeholder="Email:">
@@ -16,10 +38,7 @@
                 <input type="password" name="password" class="form-ctrl" placeholder="Password:">
             </div>
             <div class="form-btn">
-                <input type="submit" value="Login" class="btn btn-primary" class="submit">
-            </div>
-            <div class="success-message">
-                Your login was successful!
+                <input type="submit" value="Login" class="btn btn-primary" name="login">
             </div>
         </form>
     </div>
